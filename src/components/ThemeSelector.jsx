546545f07ac260106ad83c34e0fca288ef600403
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-04-17 20:41:05 -0700

    theme selector

---
+++ src/components/ThemeSelector.jsx
@@ -0,0 +1,37 @@
+const ThemeSelector = ({ theme, setTheme }) => {
+  const currentTheme = Object.keys(theme).find((key) => theme[key]);
+  return (
+    <div className='dropdown dropdown-hover dropdown-end'>
+      <div tabIndex={0} role='button' className='btn btn-xs btn-primary'>
+        <span>
+          Theme:{" "}
+          {currentTheme
+            ? currentTheme.charAt(0).toUpperCase() + currentTheme.slice(1)
+            : "Dark"}
+        </span>
+      </div>
+      <ul
+        tabIndex={0}
+        className='dropdown-content z-[1] menu p-2 shadow bg-secondary-content rounded-box w-52'
+      >
+        {Object.keys(theme).map((themeName) => (
+          <li>
+            <a
+              className={`text-primary ${
+                themeName === currentTheme
+                  ? "bg-primary-content"
+                  : "bg-transparent"
+              }`}
+              onClick={() => {
+                setTheme(themeName);
+              }}
+            >
+              {themeName.charAt(0).toUpperCase() + themeName.slice(1)}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default ThemeSelector;
