ffa89a98ab2cf57216647dbf4c75a42134faf266
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-04-18 19:08:30 -0700

    css

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -46,11 +46,11 @@ function LandingPage({ theme, handleThemeChange }) {
   return (
     <div className='bg-neutral bg-cover hue-rotate-270 h-[96vh] flex items-center justify-center overflow-x-clip'>
       <div className='flex items-center'>
-        <div className='px-10 pt-6 pb-8 flex flex-col justify-center bg-base-200  bg-opacity-100 rounded-box border-solid border-neutral-content'>
-          <div className='flex mb-1'>
+        <div className='px-12 pt-5 pb-12 flex flex-col justify-center bg-base-200  bg-opacity-100 rounded-box border-solid border-neutral-content'>
+          <div className='flex'>
             <ThemeSelector theme={theme} setTheme={handleThemeChange} />
           </div>
-          <h1 className='text-primary text-6xl font-bold text-center mb-2'>
+          <h1 className='text-primary text-[88px] font-bold text-center mt-0 mb-0'>
             Anonify
           </h1>
           <input
@@ -58,7 +58,7 @@ function LandingPage({ theme, handleThemeChange }) {
             name='playlistName'
             value={formInfo.playlistName}
             onChange={(e) => handleFormChange(e)}
-            className='input input-bordered input-neutral-content p-2 text-center mx-auto w-80 my-1'
+            className='input input-bordered input-primary text-center w-full mb-1 px-8'
             placeholder='Enter playlist name'
           />
 
@@ -73,21 +73,10 @@ function LandingPage({ theme, handleThemeChange }) {
             className='input input-bordered input-neutral-content p-2 text-center mx-auto w-96 my-1'
             placeholder='Enter song limit, 0 = None'
             />*/}
-          <button
-            disabled={generatingURL}
-            onClick={() => {
-              setGeneratingURL(true);
-              generateURL();
-            }}
-            className='btn btn-primary text-xl py-2 px-3 mt-1 mb-1 duration-500 ease-in-out w-full'
-          >
-            {generatingURL && <span class='loading loading-spinner'></span>}
-            <span>Generate URL</span>
-          </button>
           <div className='join radius-button '>
             <input
               value={generatedURL}
-              className='join-item input input-bordered input-primary flex-grow-3 p-2 text-center w-full mx-auto my-1'
+              className='join-item input input-bordered input-primary flex-grow-3 py-2 text-center w-full mx-auto my-2'
               placeholder='URL will appear here...'
             />
             <button
@@ -99,7 +88,7 @@ function LandingPage({ theme, handleThemeChange }) {
               onMouseLeave={() => {
                 setCopyClicked(false);
               }}
-              className='join-item btn btn-primary text-xl py-2 px-3 my-2 duration-500 ease-in-out rounded-btn'
+              className='join-item btn btn-primary text-xl py-2 px-3 duration-500 ease-in-out rounded-btn my-2'
             >
               <label className='swap swap-rotate'>
                 <input type='checkbox' />
@@ -112,6 +101,17 @@ function LandingPage({ theme, handleThemeChange }) {
               </label>
             </button>
           </div>
+          <button
+            disabled={generatingURL}
+            onClick={() => {
+              setGeneratingURL(true);
+              generateURL();
+            }}
+            className='btn btn-primary text-xl py-2 px-3 mt-8 mb-1 duration-500 ease-in-out w-full'
+          >
+            {generatingURL && <span class='loading loading-spinner'></span>}
+            <span>Generate URL</span>
+          </button>
         </div>
       </div>
     </div>
